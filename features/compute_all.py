fix lattice generation and normalization procedure

features/compute_all.py
@@ -26,9 +26,9 @@ def compute_feature_vectors(data, save_path):
     feature_vector = np.hstack((sop_features, rsf_features))
 
     # 2) Normalize feature fector
-    means = np.mean(feature_vector, axis=0)
-    stds = np.std(feature_vector, axis=0)
-    feature_vector = (feature_vector - means) / stds
+    # means = np.mean(feature_vector, axis=0)
+    # stds = np.std(feature_vector, axis=0)
+    # feature_vector = (feature_vector - means) / stds
 
     # 3) Save
     if save_path is None:

lattice/gen.py
@@ -69,8 +69,9 @@ class LatticeGenerator:
         # https://stackoverflow.com/questions/5408276/sampling-uniformly-distributed-random-points-inside-a-spherical-volume
         # we could potentially speed this up by sampling from a cube and rejecting points outside the sphere
         phi = np.random.uniform(0, 2 * np.pi, n_atoms)
-        theta = np.random.uniform(0, np.pi, n_atoms)
-        r = np.cbrt(np.random.uniform(0, displacement_radius, n_atoms))
+        # theta = np.random.uniform(0, np.pi, n_atoms)
+        theta = np.arccos(np.random.uniform(-1, 1, n_atoms))
+        r = displacement_radius * np.cbrt(np.random.uniform(0, 1, n_atoms))
 
         # apply displacements
         displaced_lattice.particles_.positions_[:] += np.array(

lattice/test.py
@@ -25,7 +25,7 @@ def test_np():
 
 def test_lammps():
     generator = LatticeGenerator()
-    generator.load_lammps("lattice/lammps_lattices/data/sc.gz")
+    generator.load_lammps("lattice/lammps_lattices/data/bcc.gz")
     displaced_lattices = []
     for alpha in np.linspace(0, 0.25, 5):
         displaced_lattices.append(

ml/train.py
@@ -29,7 +29,7 @@ model = MLP_Model()
 
 optim = torch.optim.Adam(
     model.parameters(),
-    lr=5e-3,
+    #lr=5e-3,
     betas=(0.9, 0.999),
 )
 
@@ -57,8 +57,8 @@ val_loader = DataLoader(
     sampler=val_sampler
 )
 
-# loss = nn.NLLLoss()
-loss = nn.CrossEntropyLoss()
+loss = nn.NLLLoss()
+# loss = nn.CrossEntropyLoss()
 
 # DEVICE
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')

ml_dataset/data_gen_single.py
@@ -20,16 +20,18 @@ def create(structure_name, fname, alpha):
     Returns:
         Nothing
     """
+    print(f"Running {structure_name} on alpha={alpha}")
 
     # Initialize generator
     generator = LatticeGenerator()
     generator.load_lammps(fname)
 
     # Generate synthetic data
-    data = []
-    for displaced_lattice in tqdm(generator.generate_range(alpha_min, alpha_max, num_lattices)):
-        data.append(compute_feature_vectors(displaced_lattice, None))
-    data = np.hstack(data)
+    # data = []
+    # # for displaced_lattice in tqdm(generator.generate_range(alpha_min, alpha_max, num_lattices)):
+    # #     data.append(compute_feature_vectors(displaced_lattice, None))
+    # data = np.hstack(data)
+    data = compute_feature_vectors(generator.generate(alpha), None)
     np.savetxt(f"ml_dataset/data/{structure_name}_{alpha}.gz", data)
 
 if __name__ == "__main__":
@@ -43,4 +45,4 @@ if __name__ == "__main__":
     create("sc", "lattice/lammps_lattices/data/sc.gz", 0, 0.25, 40)
     '''
 
-    create(sys.argv[1], sys.argv[2], float(0.25 * sys.argv[3] / 40))
+    create(sys.argv[1], sys.argv[2], np.linspace(0.01, 0.25, 10)[int(sys.argv[3])])

ml_dataset/tsne_features.py
@@ -10,25 +10,25 @@ data, labels, label_map = dataset.data, dataset.labels, dataset.label_map
 # shuffle
 indices = np.arange(len(data))
 np.random.shuffle(indices)
-data = data[indices][:1000]
-labels = labels[indices][:1000]
+data = data[indices][:5000]
+labels = labels[indices][:5000]
 
 PCA_COMPONENTS = 129
 
 def tsne(data, labels, label_map):
     # Run PCA for dimensionality reduction
-    #pca = PCA(n_components=PCA_COMPONENTS)
-    #data = pca.fit_transform(data)
-    #print("PCA complete!")
+    pca = PCA(n_components=PCA_COMPONENTS)
+    data = pca.fit_transform(data)
+    print("PCA complete!")
 
     # Run t-SNE
-    tsne = TSNE(n_components=1, random_state=0)
+    tsne = TSNE(n_components=2, random_state=0)
     data_2d = tsne.fit_transform(data)
     print("t-SNE complete!")
 
     # Plot
     plt.style.use("bmh")
-    scatter = plt.scatter(data_2d[:, 0], data_2d[:, 0], c=labels)
+    scatter = plt.scatter(data_2d[:, 0], data_2d[:, 1], c=labels)
     scatter_handles, scatter_labels = scatter.legend_elements()
     plt.legend(handles=scatter_handles, labels=label_map.keys())
     plt.show()