c4e8f8dfa60ad15f1193c64eef09c0cc435fe6e6
Alexander <acliang@mit.edu>
2025-03-23 16:06:35 -0400

    Incorperate mean and std into the model

--- ml/train.py
+++ ml/train.py
@@ -15,88 +15,100 @@ exp_name = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
 # HYPERPARAMETERS
 
-epochs = 100
-batch_size = 512
-shuffle_dataset = True
-train_val_split = 0.8
+EPOCHS = 100
+BATCH_SIZE = 512
+SHUFFLE_DATASET = True
+TRAIN_VAL_SPLIT = 0.8
 
 print("== MLP Training Script ==")
-print(f"Parameters: epochs={epochs}, batch_size={batch_size}, shuffle_dataset={shuffle_dataset}, train_val_split={train_val_split}")
+print(f"Parameters: epochs={EPOCHS}, batch_size={BATCH_SIZE}, shuffle_dataset={SHUFFLE_DATASET}, train_val_split={TRAIN_VAL_SPLIT}")
 
-# MODEL
-
-model = MLP_Model()
-
-optim = torch.optim.Adam(
-    model.parameters(),
-    #lr=5e-3,
-    betas=(0.9, 0.999),
-)
-
-# Dataset
+# DATASET
 
 dataset = CrystalDataset("ml_dataset/data")
 dataset_size = len(dataset)
 
-if shuffle_dataset:
+if SHUFFLE_DATASET:
     indices = list(range(dataset_size))
     np.random.shuffle(indices)
-split = int(np.floor(train_val_split * dataset_size))
+
+split = int(np.floor(TRAIN_VAL_SPLIT * dataset_size))
 train_sampler = SubsetRandomSampler(indices[:split])
 val_sampler = SubsetRandomSampler(indices[split:])
 
 train_loader = DataLoader(
     dataset,
-    batch_size=batch_size,
+    batch_size=BATCH_SIZE,
     sampler=train_sampler
 )
 
 val_loader = DataLoader(
     dataset,
-    batch_size=batch_size,
+    batch_size=BATCH_SIZE,
     sampler=val_sampler
 )
 
+# MODEL
+
+model = MLP_Model(dataset.means, dataset.stds)
+
+optim = torch.optim.Adam(
+    model.parameters(),
+    betas=(0.9, 0.999),
+)
+
+# LOSS
+
 loss = nn.NLLLoss()
-# loss = nn.CrossEntropyLoss()
 
 # DEVICE
+
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print(f"Using device: {device}")
 model.to(device)
+print(f"Using device: {device}")
 
 # TRAINING
+
 best_acc = 0
-for epoch in range(epochs):
+
+for epoch in range(EPOCHS):
     print(f"== Epoch {epoch} ==")
+
+    # Training
     model.train()
     tot_loss = 0
+
     for i, (x, y) in enumerate(train_loader):
         optim.zero_grad()
         y_hat = model(x.to(device).float())
-        # print(y_hat, y)
         l = loss(y_hat, y.to(device).long())
         tot_loss += l.item()
         l.backward()
         optim.step()
-        if i % 30 == 0: print(f"--> Batch {i}, Loss: {l.item()}")
 
+        if i % 30 == 0: 
+            print(f"--> Batch {i}, Loss: {l.item()}")
+
+    # Testing
     model.eval()
+
     with torch.no_grad():
         total = 0
         correct = 0
+
         for i, (x, y) in enumerate(val_loader):
             y = y.to(device)
             y_hat = model(x.to(device).float())
             _, predicted = torch.max(y_hat, 1)
             total += y.size(0)
             correct += (predicted == y).sum().item()
+        
         print(f"Epoch {epoch}, Validation Accuracy: {correct / total}")
+
         if correct / total > best_acc:
             best_acc = correct / total
-            torch.save(model.state_dict(), f"ml/models/model_best.pt")
+            torch.save(model.state_dict(), f"ml/models/model_{exp_name}.pt")
 
-    # write csv
+    # Save stats
     with open(f"ml/statistics/loss_{exp_name}.csv", "a") as f:
         f.write(f"{epoch},{tot_loss},{correct/total}\n")
-    torch.save(model.state_dict(), f"ml/models/model_{epoch}.pt")
