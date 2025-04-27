0a23d750a47ba64d4a5820136f1d036e7e73384e
Alexander Liang <alexanderliang24@mittymonarch.com>
2025-04-26 22:03:43 -0400

    dc3

--- src/dc3.py
+++ src/dc3.py
@@ -1,19 +1,18 @@
 import torch
 import numpy as np
-import pandas as pd
 from ml.model import MLP_Model
 from ovito.data import DataCollection
 from features.compute_all import compute_feature_vectors
 from outlier.coherence import calculate_amorphous
-
+from outlier.outlier_cutoffs import compute_ref_vec, compute_delta_cutoff
 
 class DC3:
     def __init__(
         self,
         model_path: str,
         label_map: dict[str, int],
-        ref_vec_path: str,
-        delta_cutoff_path: str,
+        ref_vec_folder: str,
+        synthetic_data_folder: str,
     ) -> None:
         # Model
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -32,17 +31,15 @@ class DC3:
         self.str_to_label = label_map
         self.label_to_str = {v: k for k, v in label_map.items()}
 
-        # Reference vectors
-        df = pd.read_csv(ref_vec_path)
-        self.ref_vecs = dict(zip(df["label"], df["cutoff"]))
+        # Reference vectors and delta cutoffs
+        self.ref_vecs = compute_ref_vec(ref_vec_folder, self.means, self.stds)
+        self.delta_cutoffs = compute_delta_cutoff(synthetic_data_folder, self.ref_vecs, self.means, self.stds)
 
-        # Delta cutoffs
-        df = pd.read_csv(delta_cutoff_path)
-        self.delta_cutoffs = dict(zip(df["label"], df["cutoff"]))
 
     def calculate(self, lattice: DataCollection) -> np.ndarray:
         features = compute_feature_vectors(lattice)
         amorphous = calculate_amorphous(lattice)
+        results = []
 
         with torch.no_grad():
             preds = self.model(features.to(self.device)).argmax(dim=1).cpu()
@@ -54,8 +51,8 @@ class DC3:
                 dist = np.linalg.norm(normalized_feature - ref_vec)
 
                 if dist >= self.delta_cutoffs[self.label_to_str[preds[i]]]:
-                    pass
+                    results.append("unknown")
                 else:
-                    pass
+                    results.append(preds[i])
             else:
-                pass
+                results.append("amorphous")
