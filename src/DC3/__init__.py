f63b16d61ef01af0dae45c87772f7bb7692dc0d8
Jieruei Chang <knosmos@gmail.com>
2025-04-27 12:10:05 -0400

    try to fix initializer

--- src/DC3/__init__.py
+++ src/DC3/__init__.py
@@ -15,9 +15,7 @@ class DC3Modifier(ModifierInterface):
     ref_vec_path = Str()
     delta_cutoff_path = Str()
 
-    def __init__(
-        self
-    ):
+    def __init__(self):
         """
         Initialize the DC3Modifier with the model path and label mapping.
 
@@ -28,8 +26,8 @@ class DC3Modifier(ModifierInterface):
             delta_cutoff_path (str): Path to the delta cutoffs CSV file.
         """
         # need to figure out how this works with the pipeline
-        if not model_path:
-            model_path = "ml/models/model_2025-04-26_23-04-46.pt"
+        if not self.model_path:
+            self.model_path = "ml/models/model_2025-04-26_23-04-46.pt"
         print("Initializing DC3Modifier")
         self.dc3 = DC3(
             self.model_path, self.label_map, self.ref_vec_path, self.delta_cutoff_path
@@ -46,7 +44,7 @@ class DC3Modifier(ModifierInterface):
             **kwargs: Additional keyword arguments.
         """
         # need to research how kwargs works in the pipeline
-        print("Reinitializing DC3Modifier")
-        self.dc3 = DC3(model_path, label_map, ref_vec_path, delta_cutoff_path)
+        # print("Reinitializing DC3Modifier")
+        # self.dc3 = DC3(model_path, label_map, ref_vec_path, delta_cutoff_path)
         print("Calculating structure types")
         data.particles_.create_property("Structure_Type", data=self.dc3.calculate(data))
